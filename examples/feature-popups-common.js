89cb099071452fd0cfdb6a28e736dda2c3dd338a
Xavier Mamano <xavier.mamano@gmail.com>
2012-07-25 12:46:38 +0200

    Adjustments in the examples.

--- examples/feature-popups-common.js
+++ examples/feature-popups-common.js
@@ -1,4 +1,4 @@
-// Projections
+ï»¿// Projections
 // -----------
 var sphericalMercatorProj = new OpenLayers.Projection("EPSG:900913");
 var geographicProj = new OpenLayers.Projection("EPSG:4326");
@@ -127,19 +127,19 @@ function getSprintersFeatures() {
             { "type": "Feature", "geometry": {"type": "Point", "coordinates": [-355300, 7547800]},
                 "properties": {"Name": "Jennie Fletcher ", "Country":"Scotland", "City":"Edinburgh"}},
             { "type": "Feature", "geometry": {"type": "Point", "coordinates": [657068.53608487, 5712321.2472725]},
-                "properties": {"Name": "Bruno Binet ", "Country":"France", "City":"Chambéry"}},
+                "properties": {"Name": "Bruno Binet ", "Country":"France", "City":"ChambÃ©ry"}},
             { "type": "Feature", "geometry": {"type": "Point", "coordinates": [667250.8958124, 5668048.6072737]},
                 "properties": {"Name": "Eric Lemoine", "Country":"France", "City":"Theys"}},
             { "type": "Feature", "geometry": {"type": "Point", "coordinates": [653518.03606319, 5721118.5122914]},
                 "properties": {"Name": "Antoine Abt", "Country":"France", "City":"La Motte Servolex"}},
             { "type": "Feature", "geometry": {"type": "Point", "coordinates": [657985.78042416, 5711862.6251028]},
-                "properties": {"Name": "Pierre Giraud", "Country":"France", "City":"Chambéry"}},
+                "properties": {"Name": "Pierre Giraud", "Country":"France", "City":"ChambÃ©ry"}},
             { "type": "Feature", "geometry": {"type": "Point", "coordinates": [742941.93818208, 5861818.9477535]},
-                "properties": {"Name": "Stéphane Brunner", "Country":"Switzerland", "City":"Paudex"}},
+                "properties": {"Name": "StÃ©phane Brunner", "Country":"Switzerland", "City":"Paudex"}},
             { "type": "Feature", "geometry": {"type": "Point", "coordinates": [736082.61064069, 5908165.4649505]},
-                "properties": {"Name": "Frédéric Junod", "Country":"Switzerland", "City":"Montagny-près-Yverdon"}},
+                "properties": {"Name": "FrÃ©dÃ©ric Junod", "Country":"Switzerland", "City":"Montagny-prÃ¨s-Yverdon"}},
             { "type": "Feature", "geometry": {"type": "Point", "coordinates": [771595.97057525, 5912284.7041793]},
-                "properties": {"Name": "Cédric Moullet", "Country":"Switzerland", "City":"Payerne"}},
+                "properties": {"Name": "CÃ©dric Moullet", "Country":"Switzerland", "City":"Payerne"}},
             { "type": "Feature", "geometry": {"type": "Point", "coordinates": [744205.23922364, 5861277.319748]},
                 "properties": {"Name": "Benoit Quartier", "Country":"Switzerland", "City":"Lutry"}},
             { "type": "Feature", "geometry": {"type": "Point", "coordinates": [1717430.147101, 5954568.7127565]},
